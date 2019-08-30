9c419a3e651494b848ab9cd09f04da4097dd51da
Fujinda <962408947@qq.com>
2019-08-30 12:29:04 +0800

    完成随机推荐功能

---
+++ public/assets/JS/public.js
@@ -0,0 +1,24 @@
+// 向服务器端发送请求，索要随机推荐书籍
+$.ajax({
+    type: 'get',
+    url: '/posts/random',
+    success: function(response) {
+        console.log(response);
+
+        var randomTpl = `
+            {{each data}}
+            <li>
+              <a href="detail.html?id={{$value._id}}">
+			    <p class="title">{{$value.title}}</p>
+			    <p class="reading">阅读({{$value.meta.views}})</p>
+			    <div class="pic">
+			      <img src="{{$value.thumbnail}}" alt="">
+			    </div>
+			  </a>
+            </li>
+            {{/each}}
+        `;
+        var html = template.render(randomTpl, { data: response });
+        $('#randomBox').html(html)
+    }
+})
