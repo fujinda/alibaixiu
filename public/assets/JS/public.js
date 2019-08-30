6686f23e14ef4a58b55d915b3d104482fd7727e4
Fujinda <962408947@qq.com>
2019-08-30 19:27:01 +0800

    完成搜索功能

--- public/assets/JS/public.js
+++ public/assets/JS/public.js
@@ -46,3 +46,14 @@ $.ajax({
             // $('.nav_item').html(html)
     }
 })
+
+// 获取到搜索表单 并为其添加表单提交事件
+$('.search form').on('submit', function() {
+    // alert(1)
+    // 获取到用户在表单中输入的搜索关键字
+    var keys = $(this).find('.keys').val();
+    // // 跳转到搜索结果页面 并且将用户输入的搜索关键字传递到搜索结果页面
+    location.href = "/search.html?key=" + keys
+        //     // 阻止表单默认提交行为
+    return false;
+});
